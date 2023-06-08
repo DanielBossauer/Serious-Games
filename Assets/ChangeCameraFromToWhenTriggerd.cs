98dc8d99cadcf252a9e76adaf1d5be9f5c415e1f
Daniel Bossauer <135320489+DanielBossauer@users.noreply.github.com>
2023-06-08 18:26:00 +0200

    Tutorial Begonnen
    Habe ein eine für mich erstellt Scene das Tutorial angefangen.

---
+++ Assets/ChangeCameraFromToWhenTriggerd.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraFromToWhenTriggerd : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("I got Triggerd");
+        cam1.enabled = (true);
+        cam2.enabled = (false);
+    }
+}
